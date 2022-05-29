239531e5e3e9088cb0f84931d86e70ca2a06cab5
Quimzy <bam.boy2020@gmail.com>
2022-05-29 20:20:24 +0300

    basic structure

---
+++ cod.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"archive/zip"
+	"crypto/md5"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/user"
+	"path/filepath"
+	"strings"
+	"time"
+)
+
+func checkRoot() bool {
+	currentUser, err := user.Current()
+	if err != nil {
+		log.Fatalf("[isRoot] Unable to get current user: %s", err)
+	}
+
+	return currentUser.Username == "root"
+}
+
+func getflags() (int, string) {
+	var interval = flag.Int("interval", 30, "set interval time in minutes > 1 minute")
+
+	pwd, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var dir_path = flag.String("dir", pwd, "set direcotry")
+
+	flag.Parse()
+
+	return *interval * 60, *dir_path
+}
+
+func zipper(dir_path, zip_path string) error { //yes, i copied and pasted this.. zipping in Go is too complicated
+	f, err := os.Create(zip_path)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	writer := zip.NewWriter(f)
+	defer writer.Close()
+
+	return filepath.Walk(dir_path, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		header, err := zip.FileInfoHeader(info)
+		if err != nil {
+			return err
+		}
+
+		header.Method = zip.Deflate
+
+		header.Name, err = filepath.Rel(filepath.Dir(dir_path), path)
+		if err != nil {
+			return err
+		}
+		if info.IsDir() {
+			header.Name += "/"
+		}
+
+		headerWriter, err := writer.CreateHeader(header)
+		if err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		f, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+
+		_, err = io.Copy(headerWriter, f)
+		return err
+	})
+}
+
+func md5sum(path string) string {
+	file, err := os.Open(path)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer file.Close()
+
+	hash := md5.New()
+	_, err = io.Copy(hash, file)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return string(hash.Sum(nil))
+}
+
+func cod(interval int, prevhash, dir_path, zip_path string) string {
+	zipper(dir_path, zip_path)
+	hash := md5sum(zip_path)
+	if hash == prevhash {
+		os.RemoveAll(dir_path)
+	} else {
+		return hash
+	}
+
+	return ""
+}
+
+func main() {
+	if checkRoot() {
+		interval, dir_path := getflags()
+		hash := ""
+
+		for {
+			time.Sleep(time.Duration(interval) * time.Second)
+			zip_path := fmt.Sprintf("/tmp/%s.zip", strings.Trim(dir_path, "/"))
+
+			hash = cod(interval, hash, dir_path, zip_path)
+
+		}
+	} else {
+		log.Fatal("SHOULD BE RUNNED AS ROOT (sudo ./cod)")
+	}
+
+}
